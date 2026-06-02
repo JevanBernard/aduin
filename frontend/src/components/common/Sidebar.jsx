33e684c68b100091eadc3adcc7c711d7c4220096
JevanBernard <jevan.bernard1007@gmail.com>
2026-06-02 12:12:24 +0800

    fix: import icons properly for production build

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -8,7 +8,7 @@ import laporanIcon from "../../assets/icons/laporan.svg";
 import trenIcon from "../../assets/icons/tren.svg";
 import pengaturanIcon from "../../assets/icons/pengaturan.svg";
 import logoutIcon from "../../assets/icons/logout.svg";
-import logoImg from "../../assets/logo.jpg";
+import logoImg from "../../assets/logo.svg";
 
 const MENU_ICONS = [
   { label: "Overview", src: overviewIcon, path: "/" },
